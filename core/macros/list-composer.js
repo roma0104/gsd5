bd304fe81446fe45ecc343899a080b95205e8373
Roma Hicks <roma0104@gmail.com>
2015-01-22 15:53:40 -0600

    More robust and complete gsd5-list macro.

--- core/macros/list-composer.js
+++ core/macros/list-composer.js
@@ -26,46 +26,58 @@ exports.params = [
 	{name: "sort"},
 	{name: "order"},
 	{name: "groupBy"},
+	{name: "groupTail"},
+	{name: "groupTailHeader"},
 	{name: "groupHeader"},
-	{name: "filters"}
+	{name: "filter"}
 ];
 
 /*
 Run the macro
 */
-exports.run = function(gsd_type,gsd_complete,gsd_status,realmAware,sort,order,groupBy,groupHeader,filters) {
+exports.run = function(gsd_type,gsd_complete,gsd_status,realmAware,sort,order,groupBy,groupTail,groupTailHeader,groupHeader,filter) {
 	var completeString = '',
 		statusString = '',
 		realmString = '',
 		sortString = 'nsort[' + sort + ']',
-		groupString = '';
+		groupString = '',
+		filterString = '';
 
 	console.log(typeof(groupHeader));
 	console.log(arguments);
 
 	if(gsd_type==="action"||gsd_type==="project"){
 		completeString = 'field:gsd_complete[' + gsd_complete + ']';
-		if(gsd_type==="project" && gsd_status==="next") {
-			gsd_status = "active";
-		}
 		statusString = 'field:gsd_status[' + gsd_status + ']';
 	}
 	if(realmAware==="true") {
 		realmString = 'field:gsd_realm{$:/currentRealm}';
 	}
 	if(order==="descending") {
-		sortString = '!' + sortString;
+		sortString = "!" + sortString;
 	}
-	if(groupBy!=="") {
-		groupString = 'field:' + groupBy + '{!!title}';
-	} else {
-		return '';
+	if(groupBy!=="none") {
+		groupString = 'field:' + groupBy + '{!!' + groupBy + '}';
 	}
-	if(groupHeader==='true') {
+	if(groupBy!=="none" && groupTail==="true") {
 		groupString = 'field:' + groupBy + '[]';
 	}
+	if(groupBy!=="none" && groupTailHeader==="true") {
+		groupString = 'field:' + groupBy + '[]limit[1]';
+	} else if(groupBy==="none" && groupTailHeader==="true") {
+		return "";
+	}
+	if(groupHeader==='true' && groupBy!=="none") {
+		groupString = 'has[' + groupBy + ']each[' + groupBy + ']';
+	}
+	if(groupHeader==='true' && groupBy==="none") {
+		return "";
+	}
+	if(filter!=="none") {
+		filterString = filter;
+	}
 
-	return '[field:gsd_type[' + gsd_type + ']' + completeString + statusString + realmString + sortString + filters + ']';
+	return '[field:gsd_type[' + gsd_type + ']' + completeString + statusString + realmString + sortString + groupString + filterString + ']';
 };
 
 })();

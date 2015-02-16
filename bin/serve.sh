e2436d508aa197afd6a9be90c3ed0afcfb7ba5e7
Simon Baird <simon.baird@gmail.com>
2015-02-16 23:25:18 +1000

    Add an edition file and a simple serve.sh script
    The serve.sh can serve GSD5 provided you have a nearby copy of
    the TiddlyWiki5 repo. It sets the TIDDLYWIKI_PLUGIN_PATH so the gsd5
    plugin is found directly.

---
+++ bin/serve.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+GSD5_DIR=`pwd`
+TIDDLYWIKI5_DIR=$GSD5_DIR/../TiddlyWiki5
+
+export TIDDLYWIKI_PLUGIN_PATH=$GSD5_DIR/plugins
+export TW_SERVE_EDITION_PATH=$GSD5_DIR/editions/gsd5
+( cd $TIDDLYWIKI5_DIR && ./bin/serve.sh )

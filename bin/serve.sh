a0afc5caf39a253dcb168c191e61f44673880cb6
Roma Hicks <roma0104@gmail.com>
2016-06-07 21:59:39 -0500

    Updated serving scripts to work correctly.

--- bin/serve.sh
+++ bin/serve.sh
@@ -4,5 +4,5 @@ GSD5_DIR=`pwd`
 TIDDLYWIKI5_DIR=$GSD5_DIR/../TiddlyWiki5
 
 export TIDDLYWIKI_PLUGIN_PATH=$GSD5_DIR/plugins
-export TW_SERVE_EDITION_PATH=$GSD5_DIR/editions/gsd5
+export TW_SERVE_EDITION_PATH=$GSD5_DIR/editions/gsd5-server
 ( cd $TIDDLYWIKI5_DIR && ./bin/serve.sh )

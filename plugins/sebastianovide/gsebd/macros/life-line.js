79236b9d8b6038541513849edcafec6a02e42648
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2019-07-17 18:45:40 +0100

    some tweaking to the life line

--- plugins/sebastianovide/gsebd/macros/life-line.js
+++ plugins/sebastianovide/gsebd/macros/life-line.js
@@ -18,37 +18,40 @@ Information about this macro
 exports.name = "life-line";
 
 exports.params = [
-	{name: "edge"},
-  {max: "max"}
+	{name: "dob"},
+  {name: "lifeExpectation"}
 ];
 
 /*
 Run the macro
 */
-exports.run = function(edge, max) {
-  edge = parseInt(edge)
-  max = parseInt(max)
-  if (isNaN(max)) max = 85
+exports.run = function(dob, lifeExpectation) {
+  dob = new Date(dob);
+  const millsPerYear = 365.25 * 24 * 60 * 60 * 1000;
+  const age = Math.round((new Date().getTime() - dob.getTime()) / millsPerYear);
+  lifeExpectation = parseInt(lifeExpectation)
+  if (isNaN(lifeExpectation)) lifeExpectation = 85
 
   let witiText = "`???`";
   
-  if (!isNaN(edge) && edge >= 0) {
-    if (max < edge) max = edge
-    let edgeLine = "";
+  if (!isNaN(age) && age >= 0 && age < 100 && lifeExpectation >=0 && lifeExpectation < 100) {
+    if (lifeExpectation < age) lifeExpectation = age
+    let ageLine = "";
     let decadeLine = "";
-    for (let i=0; i <= max; i++) {
+    for (let i=0; i <= lifeExpectation; i++) {
       const decade = parseInt(i/10)
 
-      if (i < edge) edgeLine += "X"
-      else if (i == edge) edgeLine += "!"
-      else if (i == decade * 10) edgeLine += "+"
-      else edgeLine += "-"
+      if (i < age && i == decade * 10) ageLine += "╤"
+      else if (i < age) ageLine += "═"
+      else if (i == age) ageLine += "▷"
+      else if (i == decade * 10) ageLine += "┬"
+      else ageLine += "─"
       
       if (decade == i/10) decadeLine += decade * 10;
       else if (i > decade * 10 + 1 ) decadeLine += " "
     }
-    if (max != parseInt(max/10)*10) decadeLine += " "
-    witiText = "`" + edgeLine + "`<br>`" + decadeLine + "`<br>";
+    if (lifeExpectation != parseInt(lifeExpectation/10)*10) decadeLine += " "
+    witiText = "`" + ageLine + "`<br>`" + decadeLine + "`<br>";
   }
   
 	return witiText;

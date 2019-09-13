c639c0119c0d1b0988a196b02ce273218cc8103a
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2019-09-14 00:23:16 +0100

    the patch version will become 0 with the package version minor or major changes

--- bin/updateVersion.js
+++ bin/updateVersion.js
@@ -1,30 +1,47 @@
 #!/usr/bin/env node
 
-// combine version from package and TRAVIS_BUILD_NUMBER
+const git = require('simple-git/promise');
 const packageJson = require("./../package.json");
-const version = packageJson.version;
-const buildNumber = process.env.TRAVIS_BUILD_NUMBER || new Date().getTime();
-
-const versionArray = version.split(".");
-versionArray[2] = buildNumber;
-const tiddlywikiVersion = packageJson.dependencies.tiddlywiki;
-
-// write version in plugin.info
+const semver = require('semver')
 const fs = require('fs');
-const pluginInfoOutputPath = "./plugin/plugin.info";
-const pluginInfoInputPath = "./plugin.info";
-const pluginInfo = JSON.parse(fs.readFileSync(pluginInfoInputPath));
-pluginInfo.version = versionArray.join(".");
-pluginInfo["core-version"] = tiddlywikiVersion.replace("^", ">=");
-pluginInfo.description = packageJson.description;
-pluginInfo.author = packageJson.author;
 
-fs.writeFileSync(pluginInfoOutputPath, JSON.stringify(pluginInfo, "", 2));
+(async ()  => {
+  const versions = (await git('.').tag()).split('\n').filter(tag => semver.clean(tag));
 
-// save the version for git tagging
-const dir = './build';
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+  const version = makeVersion(versions);
+  fs.writeFileSync("./build/APP_VERSION", version);
+  writePluginInfo(version);
+})()
+
+function makeVersion(versions) {
+  versions = versions.sort(semver.rcompare)
+  const biggestVersion = versions[0]
+
+  // find new version
+  let version = semver.inc(biggestVersion, "patch");
+  if (semver.gt(packageJson.version, biggestVersion)) {
+    version = packageJson.version
+  }
+
+  // in dev it may be usefull to have always a bigger version so that it can be upgraded
+  if (!process.env.TRAVIS_BUILD_NUMBER ) {
+    version = `${semver.major(version)}.${semver.minor(version)}.${new Date().getTime()}`
+  }
+
+  return version;
 }
-fs.writeFileSync(dir + "/APP_VERSION", pluginInfo.version);
-console.log("Building version: ", pluginInfo.version);
+
+function writePluginInfo(version) {
+  const tiddlywikiVersion = packageJson.dependencies.tiddlywiki;
+
+  // write version in plugin.info
+  const pluginInfoInputPath = "./plugin.info";
+  const pluginInfoOutputPath = "./plugin/plugin.info";
+  const pluginInfo = JSON.parse(fs.readFileSync(pluginInfoInputPath));
+  pluginInfo.version = version;
+  pluginInfo["core-version"] = tiddlywikiVersion.replace("^", ">=");
+  pluginInfo.description = packageJson.description;
+  pluginInfo.author = packageJson.author;
+
+  fs.writeFileSync(pluginInfoOutputPath, JSON.stringify(pluginInfo, "", 2));
+}
